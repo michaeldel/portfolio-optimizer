0ec2604d4bde81c9ad8056b02bd05c6959d11761
michaeldel <michaeldel@protonmail.com>
2017-12-28 18:53:22 +0100

    Update Eigen version to last development one

--- scripts/install_eigen.sh
+++ scripts/install_eigen.sh
@@ -9,9 +9,9 @@ if [ ! -d "$1" ]; then
   exit 1
 fi
 
-EIGEN_TARGZ_URL="http://bitbucket.org/eigen/eigen/get/3.3.4.tar.gz"
+EIGEN_TARGZ_URL="http://bitbucket.org/eigen/eigen/get/default.tar.gz"
 echo $EIGEN_TARGZ_URL
 
 cd $1
 # mkdir Eigen
-curl -SL $EIGEN_TARGZ_URL | tar xvz "eigen-eigen-5a0156e40feb/Eigen" --strip-components=1
+curl -SL $EIGEN_TARGZ_URL | tar xvz --wildcards "eigen-eigen-*/Eigen" --strip-components=1
